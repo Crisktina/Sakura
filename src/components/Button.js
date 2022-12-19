983abcd509c13246ab9fedbbdcf7a3bcc64001ff
Crisktina <crishidalgolopez@gmail.com>
2022-12-19 12:12:21 +0100

    route inside button from header

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Button() {
-  return <>{/* <Link to="/lectura">Mi lectura</Link> */}</>;
+  return (
+    <>
+      <Link to="/lectura">Mi lectura</Link>
+    </>
+  );
 }
 
 export default Button;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Main from "../components/Main";
 import Header from "../components/Header";
 
@@ -8,7 +7,6 @@ function Home() {
     <>
       <Header />
       <Main />
-      <Link to="/lectura">Descubrir cartas</Link>
     </>
   );
 }
