ed39fb789515c85f69e6b512ea829772f1f018d3
KaroVillarraga <karovilla@gmail.com>
2022-12-19 10:08:07 +0100

    print cards without no image cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,25 +6,30 @@ function Main() {
   const [cards, setCards] = useState(null)
 
   useEffect(()=>{
-    GetCards(setCards)
+    GetCards(setCards);
   },[])
 
   return (
     <>
-      <div className= 'container'>
-        {cards !== null ? (
-        cards.map(card => (
-          
-            <div key={card.id}>
-                <a href={`${card.id}`}>{card.id}</a>
-                <img src={card.cardsReverse.clowReverse} alt={card.spanishName}/>
-            </div>
-         
-        ))
-            ) : ('No hay cards')}
-    </div>
+  <div className="container">
+        {cards !== []
+          ? cards.map(card => {
+              if (card.cardsReverse.clowReverse) {
+                return (
+                  <div key={card.id}>
+                    <img
+                      src={card.cardsReverse.clowReverse}
+                      alt={card.spanishName}
+                    />
+                  </div>
+                );
+              }
+              return <div style={{ display: "none" }}></div>;
+            })
+          : "No hay cards"}
+      </div>
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
