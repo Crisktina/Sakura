17c9ac095d8d64f24472bed3a062deeb6d2649d3
Crisktina <crishidalgolopez@gmail.com>
2022-12-21 10:37:54 +0100

    random cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,6 @@ import Cards from "./Cards";
 
 function Main({ selectedCards, setSelectedCards }) {
   const [cards, setCards] = useState([]);
-
   useEffect(() => {
     GetCards(setCards);
   }, []);
